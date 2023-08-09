74c435af0b91ef26b12ec0b33e7b02c57b9b27ad
khalid4447 <khalid4447@gmail.com>
2023-08-10 01:48:40 +0300

    keypad html

---
+++ keypad/script.js
@@ -0,0 +1,16 @@
+document.getElementById("forward").addEventListener("click", function() {
+    // Code to send forward command
+});
+
+document.getElementById("backward").addEventListener("click", function() {
+    // Code to send backward command
+});
+
+document.getElementById("left").addEventListener("click", function() {
+    // Code to send left command
+});
+
+document.getElementById("right").addEventListener("click", function() {
+    // Code to send right command
+});
+
